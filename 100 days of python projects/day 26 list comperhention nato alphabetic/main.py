037f89e5474b6b74effebd9bcaa4ffc2d9be7116
ben-ka <ben10katz@gmail.com>
2023-06-17 00:11:24 +0300

    Update main.py added exceptions

--- 100 days of python projects/day 26 list comperhention nato alphabetic/main.py
+++ 100 days of python projects/day 26 list comperhention nato alphabetic/main.py
@@ -3,8 +3,14 @@ import pandas
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
 
 data_dict = {row.letter:row.code for (index,row) in data.iterrows()}
-
-word = input('insert word: ').upper()
-letters = word.strip('')
-nato_letters = [data_dict[letter] for letter in letters]
-print(nato_letters)
+is_on = True
+while is_on:
+    word = input('Enter word: ').upper()
+    letters = word.strip('')
+    try:
+        nato_letters = [data_dict[letter] for letter in letters]
+    except KeyError:
+        print('Invalid word. Only letters in the Alphabet please')
+    else:
+        print(nato_letters)
+        is_on = False
